2b6f7c9437537e5a7da4847629a9cd6d43305ef9
unknown <lucasmalucha@gmail.com>
2019-07-05 20:02:35 +0100

    store

--- app.py
+++ app.py
@@ -2,8 +2,46 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, world!"
+stores = [
+    {
+        'name': "My Store",
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+
+            }
+
+        ]
+
+    }
+
+]
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+@app.route('/stores')
+def get_stores():
+    pass
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
 
 app.run()
