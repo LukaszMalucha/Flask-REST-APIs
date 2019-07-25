e70bf7f8f4bbc9df8ab6ac26743fdf55ae67f016
unknown <lucasmalucha@gmail.com>
2019-07-25 07:26:03 +0100

    postgres

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ from ma import ma
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('POSTGRES_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # do not track changes until saved
 app.config['PROPAGATE_EXCEPTIONS'] = True  # flask extensions can raise their own errors
 app.config['JWT_BLACKLIST_ENABLED'] = True  # enable blacklisting user id's
